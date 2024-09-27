a9f2ab9d717599aebc8d7db29129b8772b4c8f1a
zhiqiangZhou <zhiqiang.zhou@voltageenergy.com>
2024-09-27 17:28:15 +0800

    update

--- Assets/Script/20240910Test/RigidBodyColliderDrive.cs
+++ Assets/Script/20240910Test/RigidBodyColliderDrive.cs
@@ -8,8 +8,10 @@ using UnityEngine.XR.Interaction.Toolkit.Utilities;
 using UnityEngine.XR.Interaction.Toolkit;
 
 
-
-public class RigidBodyColliderDrive : MonoBehaviour
+/// <summary>
+/// 修改自CharacterControllerDriver，根据VR相机的高度来设置VR模块主要CapsuleCollider的高度和位置
+/// </summary>
+public class RigidbodyColliderDrive : MonoBehaviour
 {
     [SerializeField] private Rigidbody m_Rigidbody;
     [SerializeField]
@@ -17,11 +19,11 @@ public class RigidBodyColliderDrive : MonoBehaviour
     private LocomotionProvider m_LocomotionProvider;
 
     [SerializeField]
-    [Tooltip("The minimum height of the character's capsule that will be set by this behavior.")]
+    [Tooltip("The minimum height of the CapsuleCollider that will be set by this behavior.")]
     private float m_MinHeight;
 
     [SerializeField]
-    [Tooltip("The maximum height of the character's capsule that will be set by this behavior.")]
+    [Tooltip("The maximum height of the CapsuleCollider that will be set by this behavior.")]
     private float m_MaxHeight = float.PositiveInfinity;
 
     private XROrigin m_XROrigin;
@@ -76,8 +78,8 @@ public class RigidBodyColliderDrive : MonoBehaviour
     protected CapsuleCollider capsuleCollider => m_capsuleCollider;
 
 
-    [Obsolete("xrRig has been deprecated. Use xrOrigin instead.")]
-    protected XRRig xrRig => xrOrigin as XRRig;
+    // [Obsolete("xrRig has been deprecated. Use xrOrigin instead.")]
+    // protected XRRig xrRig => xrOrigin as XRRig;
 
     protected void Awake()
     {
@@ -90,7 +92,7 @@ public class RigidBodyColliderDrive : MonoBehaviour
         if (m_LocomotionProvider == null)
         {
 
-            Debug.LogWarning("Unable to drive properties of the Character Controller without the locomotion events of a Locomotion Provider. Set Locomotion Provider or ensure a Continuous Move Provider component is in your scene.", this);
+            Debug.LogWarning("without the locomotion events of a Locomotion Provider. Set Locomotion Provider or ensure a Continuous Move Provider component is in your scene.", this);
 
         }
         try
@@ -102,7 +104,6 @@ public class RigidBodyColliderDrive : MonoBehaviour
             Debug.LogError(e.Message);
             if (m_Rigidbody == null)
             {
-                //Debug.Log("Open \"m_EnableMoveWithRigidbod\",but None Rigidbody Component");
                 Debug.LogError("Please Add Rigidbody Component:Open \"m_EnableMoveWithRigidbod\",but None Rigidbody Component");
             }
         }
@@ -126,14 +127,14 @@ public class RigidBodyColliderDrive : MonoBehaviour
         {
             Debug.LogError("RigidBodyColliderDrive lose m_XROrigin", this);
         }
-        //m_CharacterController = ((m_XROrigin != null) ? m_XROrigin.Origin.GetComponent<CharacterController>() : null);
+        
         m_capsuleCollider = this.GetComponent<CapsuleCollider>();
         if (m_capsuleCollider == null)
         {
             Debug.LogError("RigidBodyColliderDrive lose m_capsuleCollider");
             
         }
-        //SetupCharacterController();
+        
         UpdateRigidBodyCollider();
         
     }
@@ -144,14 +145,7 @@ public class RigidBodyColliderDrive : MonoBehaviour
 
     protected virtual void UpdateRigidBodyCollider()
     {
-        // if (!(m_XROrigin == null) && !(m_CharacterController == null))
-        // {
-        //     float num = Mathf.Clamp(m_XROrigin.CameraInOriginSpaceHeight, m_MinHeight, m_MaxHeight);
-        //     Vector3 cameraInOriginSpacePos = m_XROrigin.CameraInOriginSpacePos;
-        //     cameraInOriginSpacePos.y = num / 2f + m_CharacterController.skinWidth;
-        //     m_CharacterController.height = num;
-        //     m_CharacterController.center = cameraInOriginSpacePos;
-        // }
+       
         if (!(m_XROrigin == null) && !(m_capsuleCollider == null))
         {
             float num = Mathf.Clamp(m_XROrigin.CameraInOriginSpaceHeight, m_MinHeight, m_MaxHeight);
@@ -182,18 +176,7 @@ public class RigidBodyColliderDrive : MonoBehaviour
         }
     }
 
-    // private void SetupCharacterController()
-    // {
-    //     if (!(m_LocomotionProvider == null) && !(m_LocomotionProvider.system == null))
-    //     {
-    //         m_XROrigin = m_LocomotionProvider.system.xrOrigin;
-    //         m_CharacterController = ((m_XROrigin != null) ? m_XROrigin.Origin.GetComponent<CharacterController>() : null);
-    //         if (m_CharacterController == null && m_XROrigin != null)
-    //         {
-    //             Debug.LogError($"Could not get CharacterController on {m_XROrigin.Origin}, unable to drive properties." + $" Ensure there is a CharacterController on the \"Rig\" GameObject of {m_XROrigin}.", this);
-    //         }
-    //     }
-    // }
+    
 
     private void OnBeginLocomotion(LocomotionSystem system)
     {
